8f274d94bd62d3f5dc0c8848956aaf6078fde836
mattiapuglisi099@gmail.com <mattiapuglisi099@gmail.com>
2022-05-03 16:44:59 +0200

    create

---
+++ resources/views/admin/posts/create.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app');
+
+@section('content')
+<div class="container">
+  <div class="row justify-content-center">
+      <div class="col-md-8">
+          <div class="card">
+              <div class="card-header"><h3>Dashboard</h3></div>
+
+              <div class="card-body">
+                @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                @endif
+              
+                <form>
+                  <div class="mb-3">
+                    <label for="exampleInputEmail1" class="form-label">Email address</label>
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                  </div>
+                  <div class="mb-3">
+                    <label for="exampleInputPassword1" class="form-label">Password</label>
+                    <input type="password" class="form-control" id="exampleInputPassword1">
+                  </div>
+                  <div class="mb-3 form-check">
+                    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+                    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                  </div>
+                  <button type="submit" class="btn btn-primary">Submit</button>
+                </form>
+              </div>
+          </div>
+      </div>
+  </div>
+</div>
+@endsection
